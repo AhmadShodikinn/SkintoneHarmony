b458beada8c8ec44ca0052f05206461c98e90150
AhmadShodikinn <dikiahmad05@gmail.com>
2024-05-30 14:59:16 +0700

    added mainActivity & progress uploadActivity

---
+++ app/src/main/java/com/example/skintoneharmony/UploadImage.kt
@@ -0,0 +1,25 @@
+package com.example.skintoneharmony
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.skintoneharmony.databinding.ActivityUploadImageBinding
+
+class UploadImage : AppCompatActivity() {
+    private lateinit var binding : ActivityUploadImageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.upload)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
