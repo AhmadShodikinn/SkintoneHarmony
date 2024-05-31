d84cc751b681fa3f320eca3b26c2f987e166fcf5
AhmadShodikinn <dikiahmad05@gmail.com>
2024-05-31 11:24:07 +0700

    Added progress uploadActivity

--- app/src/main/java/com/example/skintoneharmony/UploadImage.kt
+++ app/src/main/java/com/example/skintoneharmony/UploadImage.kt
@@ -2,6 +2,8 @@ package com.example.skintoneharmony
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +22,27 @@ class UploadImage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setSupportActionBar(binding.myToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)  // Inflate the menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_setting -> {
+            // User chooses the "Settings" item. Show the app settings UI.
+            true
+        }
+
+        else -> {
+            // The user's action isn't recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
         }
     }
 }
