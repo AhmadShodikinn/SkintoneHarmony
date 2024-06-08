e51c74d706c5e540f7ad9e37c3d8986e86c821ca
AhmadShodikinn <dikiahmad05@gmail.com>
2024-06-08 12:19:21 +0700

    fixing layout splashscreen, final merge

--- app/src/main/java/com/example/skintoneharmony/SplashScreen.kt
+++ app/src/main/java/com/example/skintoneharmony/SplashScreen.kt
@@ -18,6 +18,13 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         window.insetsController?.hide(WindowInsets.Type.statusBars())
 
         Handler(Looper.getMainLooper()).postDelayed({
