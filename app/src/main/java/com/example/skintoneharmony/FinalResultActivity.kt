6333d9f588b6f85c683df95627b20faeeb7b2374
ihsanharimurti <harimurti114@gmail.com>
2024-06-10 08:49:20 +0700

    Implementing ViewModel for API fetching

--- app/src/main/java/com/example/skintoneharmony/FinalResultActivity.kt
+++ app/src/main/java/com/example/skintoneharmony/FinalResultActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +16,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.skintoneharmony.data.response.SkintoneResponseItem
 import com.example.skintoneharmony.data.retrofit.ApiConfig
 import com.example.skintoneharmony.databinding.ActivityFinalResultBinding
+import com.example.skintoneharmony.viewmodel.MainViewModel
+import com.example.skintoneharmony.viewmodel.ViewModelFactory
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,22 +26,32 @@ class FinalResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFinalResultBinding
     private lateinit var adapter: ShadeAdapter
 
-    companion object {
-        private const val TAG = "FinalResultActivity"
-        private const val SKINTONE = "FAIR"
+    val factory: ViewModelFactory by lazy {
+        ViewModelFactory.getInstance(application)
     }
+    val mainViewModel: MainViewModel by viewModels {
+        factory
+    }
+//    companion object {
+//        const val TAG = "FinalResultActivity"
+//        const val SKINTONE = "FAIR"
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFinalResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mainViewModel.listTone.observe(this) { listTone ->
+            setShadeData(listTone)
+        }
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mainViewModel.findShades()
 
         setSupportActionBar(binding.myToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
@@ -52,7 +65,8 @@ class FinalResultActivity : AppCompatActivity() {
         adapter = ShadeAdapter()
         binding.recyclerView.adapter = adapter
 
-        findShades()
+
+//        findShades()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -69,37 +83,37 @@ class FinalResultActivity : AppCompatActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
-    private fun findShades() {
-        val client = ApiConfig.getApiService().getShades(SKINTONE)
-        client.enqueue(object : Callback<List<SkintoneResponseItem>> {
-            override fun onResponse(
-                call: Call<List<SkintoneResponseItem>>,
-                response: Response<List<SkintoneResponseItem>>
-            ) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setReviewData(responseBody)
-                    } else {
-                        Log.e(TAG, "Response Body is null")
-                        Toast.makeText(this@FinalResultActivity, "Response Body is null", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Log.e(TAG, "Error Response: ${response.message()}")
-                    Log.e(TAG, "Error Code: ${response.code()}")
-                    Log.e(TAG, "Error Body: ${response.errorBody()?.string()}")
-                    Toast.makeText(this@FinalResultActivity, "Error Response: ${response.message()}", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onFailure(call: Call<List<SkintoneResponseItem>>, t: Throwable) {
-                Log.e(TAG, "onFailure: ${t.message}")
-                Toast.makeText(this@FinalResultActivity, "Request failed: ${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    private fun setReviewData(consumerReviews: List<SkintoneResponseItem>) {
-        adapter.submitList(consumerReviews)
+//    private fun findShades() {
+//        val client = ApiConfig.getApiService().getShades(SKINTONE)
+//        client.enqueue(object : Callback<List<SkintoneResponseItem>> {
+//            override fun onResponse(
+//                call: Call<List<SkintoneResponseItem>>,
+//                response: Response<List<SkintoneResponseItem>>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val responseBody = response.body()
+//                    if (responseBody != null) {
+//                        setShadeData(responseBody)
+//                    } else {
+//                        Log.e(TAG, "Response Body is null")
+//                        Toast.makeText(this@FinalResultActivity, "Response Body is null", Toast.LENGTH_SHORT).show()
+//                    }
+//                } else {
+//                    Log.e(TAG, "Error Response: ${response.message()}")
+//                    Log.e(TAG, "Error Code: ${response.code()}")
+//                    Log.e(TAG, "Error Body: ${response.errorBody()?.string()}")
+//                    Toast.makeText(this@FinalResultActivity, "Error Response: ${response.message()}", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<SkintoneResponseItem>>, t: Throwable) {
+//                Log.e(TAG, "onFailure: ${t.message}")
+//                Toast.makeText(this@FinalResultActivity, "Request failed: ${t.message}", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+//    }
+//
+    private fun setShadeData(listTone: List<SkintoneResponseItem>) {
+        adapter.submitList(listTone)
     }
 }
--- app/src/main/java/com/example/skintoneharmony/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/skintoneharmony/viewmodel/MainViewModel.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.skintoneharmony.FinalResultActivity
+import com.example.skintoneharmony.ShadeAdapter
 import com.example.skintoneharmony.data.response.SkintoneResponse
 import com.example.skintoneharmony.data.response.SkintoneResponseItem
 import com.example.skintoneharmony.data.retrofit.ApiConfig
@@ -16,13 +18,17 @@ import retrofit2.Response
 
 class MainViewModel (application: Application) : ViewModel(){
     private val _listTone = MutableLiveData<List<SkintoneResponseItem>>()
-    private lateinit var finalBinding: ActivityFinalResultBinding
-
     val listTone: LiveData<List<SkintoneResponseItem>> = _listTone
 
+//    private lateinit var finalBinding: ActivityFinalResultBinding
 
-    companion object{
-        private const val TAG = "MainViewModel"
+    private lateinit var adapter: ShadeAdapter
+
+
+
+    companion object {
+        const val TAG = "FinalResultActivity"
+        private const val SKINTONE = "FAIR"
     }
 
 //    init{
@@ -46,5 +52,39 @@ class MainViewModel (application: Application) : ViewModel(){
 //            Log.d("Token e", e.toString())
 //        }
 //
+//    }
+
+    fun findShades() {
+        val client = ApiConfig.getApiService().getShades(SKINTONE)
+        client.enqueue(object : Callback<List<SkintoneResponseItem>> {
+            override fun onResponse(
+                call: Call<List<SkintoneResponseItem>>,
+                response: Response<List<SkintoneResponseItem>>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _listTone.value = responseBody
+                    } else {
+                        Log.e(TAG, "Response Body is null")
+//                        Toast.makeText(this@FinalResultActivity, "Response Body is null", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Log.e(TAG, "Error Response: ${response.message()}")
+                    Log.e(TAG, "Error Code: ${response.code()}")
+                    Log.e(TAG, "Error Body: ${response.errorBody()?.string()}")
+//                    Toast.makeText(this@FinalResultActivity, "Error Response: ${response.message()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<SkintoneResponseItem>>, t: Throwable) {
+                Log.e(TAG, "onFailure: ${t.message}")
+//                Toast.makeText(this@FinalResultActivity, "Request failed: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+//    private fun setShadeData(consumerReviews: List<SkintoneResponseItem>) {
+//        adapter.submitList(consumerReviews)
 //    }
 }
